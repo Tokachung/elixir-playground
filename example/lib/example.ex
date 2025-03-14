68277b4aafb66f4a715fd5a813b0a0e86d86d978
tokachung <st1403@live.com>
2025-03-14 19:39:03 +1100

    time until new year calculator

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -8,14 +8,20 @@ defmodule Example do
   end
 
   def main do
-    name = "Caleb";
-    status = Enum.random([:gold, :silver, :bronze, :banned])
+    time = DateTime.new!(Date.new!(2026, 1, 1), Time.new!(0,0,0,0), "Etc/UTC")
+    time_till = DateTime.diff(time, DateTime.utc_now())
+    IO.puts(time_till)
 
-    case status do
-      :gold -> IO.puts("Welcome to the fancy lounge, #{name}")
-      :banned -> IO.puts("You are banned, #{name}")
-      _ -> IO.puts("You are a peasant, #{name}")
-    end
+    days = div(time_till, 86400) # Calculate days
+    IO.puts(days)
+    hours = div(rem(time_till, 86400), 60 * 60) #
+    IO.puts(hours)
+    mins = div(rem(time_till, 3600), 60)
+    IO.puts(mins)
+    seconds = rem(time_till, 60)
+    IO.puts(seconds)
+
+    IO.puts("Time until new year: #{days} days, #{hours} hours, #{mins} minutes, #{seconds} seconds")
   end
 end
 
